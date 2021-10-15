73462c824579207ecf2e8d08c08f2a255da5db71
Xyra1999 <xyraysalinaamadeo@gmail.com>
2021-10-15 23:31:34 +0800

    Thesis

---
+++ main/form.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/form_box.css">
+</head>
+<body>
+
+<div class="sign_box">
+		<div class="sign_content">
+			<center><h3>Create Account</h3></center>
+			<form action="signup/signup" autocomplete="off" method="post">
+				<div class="name">
+					<div class="input">
+						<div class="input_flex">
+							<div class="input-flex">
+							<div class="input_list">
+							<label for="">Email Address</label>
+							<br>
+							<input type="text" placeholder="" name="email">
+						</div>
+							<div class="input_list">
+							<label for="">Last Name</label>
+							<br>
+							<input type="text" placeholder="" autocomplete="off" name="lname">
+							<div class="input_list">
+							<label for="">First Name</label>
+							<br>
+							<input type="text" placeholder="" name="fname">
+						</div>
+						<div class="input_list">
+							<label for="">Middle Name</label>
+							<br>
+							<input type="text" placeholder="" name="mname">
+						</div>
+						<div class="input_flex">
+							<div class="input_list">
+							<label for="">Age</label>
+							<br>
+							<input type="text" placeholder="" name="age">
+						</div>
+						
+						</div>
+						<div class="input-flex">
+							<div class="input_list">
+							<label for="">Gender</label>
+							<br>
+							<input type="text" placeholder="" name="gender">
+						</div>
+						</div>
+						<div class="input-flex">
+							<div class="input_list">
+							<label for="">Birth Date</label>
+							<br>
+							<input type="text" placeholder="" name="birthdate">
+						</div>
+						<div class="input-flex">
+							<div class="input_list">
+							<label for="">Date of Place</label>
+							<br>
+							<input type="text" placeholder="" name="place_birth">
+						</div>
+						<div class="input_flex">
+							<div class="input_list">
+							<label for="">Contact Number</label>
+							<br>
+							<input type="text" placeholder="" name="contact">
+							</div>
+						</div>
+						<div class="input-flex">
+							<div class="input_list">
+							<label for="">Password</label>
+							<br>
+							<input type="text" placeholder="" name="password">
+						</div>
+						<div class="input-flex">
+							<div class="input_list">
+							<label for="">Position</label>
+							<br>
+							<input type="text" placeholder="" name="eadd">
+						</div>
+						
+
+
+													
+							<div class="btn">
+								<center><button type="submit" name="Register">Submit</button></center>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ main/user.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User </title>
+	<link rel="stylesheet" href="css/user.css">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="content">
+    <div class="sidebar">
+        <h2>Dashboard</h2>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Notification</a></li>
+            <li><a href="#">Form</a></li>
+            <li><a href="#">Document</a></li>
+            <li><a href="#">Resources</a></li>
+            <li><a href="#">Fsuu Website</a></li>
+            <li><a href="index">Logout</a></li>
+        </ul> 
+    </div>
+    <div class="main_content">
+        <div class="header">
+        	<p><b>Name:</b> <span>Alyssa Ave</span></p>
+
+        </div>  
+        <div class="info">
+         	<p><!-- Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum? Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum? Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum?</p>
+         	<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum? Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum? Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Illum placeat perspiciatis repellat eveniet quod, molestiae veniam aperiam in, reiciendis possimus laudantium nihil, sunt enim quam voluptate iure officiis impedit maiores! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam molestiae rem quisquam rerum numquam sed similique architecto eveniet adipisci corrupti amet libero repellendus hic facere, nam aut mollitia earum?  --></p>
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
